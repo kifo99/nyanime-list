b17e320f27947706e384cbed9f36cc704d9331fa
kifo99 <ivan123rakovic@gmail.com>
2025-01-31 14:55:14 +0100

    Setting up server side of the nyanime-list

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import bodyParser from "body-parser";
+
+import { PORT } from "./util/config.js";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors({ origin: "http://localhost:3000/" }));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+});
+
+app.use(limiter);
+
+app.listen(PORT || 8000);
