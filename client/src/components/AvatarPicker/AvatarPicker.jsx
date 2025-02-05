cac12385ddbc0bfacb0792a4fc9352d18c2fc449
kifo99 <ivan123rakovic@gmail.com>
2025-02-05 16:09:51 +0100

    Addint changes to AnimePicker

---
+++ client/src/components/AvatarPicker/AvatarPicker.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+export default function AvatarPicker() {
+  //   const name = "gambit";
+  //   useEffect(function () {
+  //     async function fetchData() {
+  //       const data = await axios.get(
+  //         `https://api.dicebear.com/9.x/initials/svg?seed=Felix`
+  //       );
+
+  //       console.log(data);
+  //     }
+
+  //     fetchData();
+  //   });
+  return <h1>Neko cats</h1>;
+}
