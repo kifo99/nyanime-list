13230dfa234ff5c506c73e9cdf90c935c90d5fe0
kifo99 <ivan123rakovic@gmail.com>
2025-02-05 17:14:54 +0100

    Pushing update of AnimePicker component

--- client/src/components/AvatarPicker/AvatarPicker.jsx
+++ client/src/components/AvatarPicker/AvatarPicker.jsx
@@ -1,18 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function AvatarPicker() {
+  const [svg, setSvg] = useState("");
   const name = "gambit";
-  useEffect(function () {
-    async function fetchData() {
-      const data = await axios.get(
-        `http://localhost:8080/anime/avatar/${name}`
-      );
+  useEffect(
+    function () {
+      async function fetchData() {
+        const { data } = await axios.get(
+          `http://localhost:8080/anime/avatar/${name}`
+        );
 
-      console.log(data);
-    }
+        setSvg(data.svg);
+      }
 
-    fetchData();
-  });
-  return <h1>Neko cats</h1>;
+      fetchData();
+    },
+    [name]
+  );
+  return <div dangerouslySetInnerHTML={{ __html: svg }} />;
 }
