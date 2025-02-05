527b6ec8f7cad4e931e697909e424002a432603c
kifo99 <ivan123rakovic@gmail.com>
2025-02-05 17:04:54 +0100

    Commiting updates done in anime controller

--- server/routes/anime.js
+++ server/routes/anime.js
@@ -5,7 +5,7 @@ import { getAnime, getAnimeWithId, getAvatar } from "../controller/anime.js";
 const router = express.Router();
 
 router.get("/select/:animeId", getAnimeWithId);
-router.get("/search/anime/:name", getAnime);
 router.get("/avatar/:name", getAvatar);
+router.get("/search/anime/:name", getAnime);
 
 export default router;
