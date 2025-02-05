d672339026a00a70874949f6e258d4ad849d94b7
kifo99 <ivan123rakovic@gmail.com>
2025-02-05 16:19:30 +0100

    Adding new route and new controller function i anime route and controller

--- server/routes/anime.js
+++ server/routes/anime.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { getAnime, getAnimeWithId } from "../controller/anime.js";
+import { getAnime, getAnimeWithId, getAvatar } from "../controller/anime.js";
 
 const router = express.Router();
 
 router.get("/select/:animeId", getAnimeWithId);
 router.get("/search/anime/:name", getAnime);
+router.get("/avatar/:name", getAvatar);
 
 export default router;
