60cc6132a4dbd57731a52907590adef50bbb2c01
kifo99 <ivan123rakovic@gmail.com>
2025-02-05 16:23:28 +0100

    Updating AnimePicker

--- client/src/components/AvatarPicker/AvatarPicker.jsx
+++ client/src/components/AvatarPicker/AvatarPicker.jsx
@@ -2,17 +2,17 @@ import { useEffect } from "react";
 import axios from "axios";
 
 export default function AvatarPicker() {
-  //   const name = "gambit";
-  //   useEffect(function () {
-  //     async function fetchData() {
-  //       const data = await axios.get(
-  //         `https://api.dicebear.com/9.x/initials/svg?seed=Felix`
-  //       );
+  const name = "gambit";
+  useEffect(function () {
+    async function fetchData() {
+      const data = await axios.get(
+        `http://localhost:8080/anime/avatar/${name}`
+      );
 
-  //       console.log(data);
-  //     }
+      console.log(data);
+    }
 
-  //     fetchData();
-  //   });
+    fetchData();
+  });
   return <h1>Neko cats</h1>;
 }
