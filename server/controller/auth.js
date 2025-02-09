8e4e86100dca73ccc759f9a2d9854563433c7022
kifo99 <ivan123rakovic@gmail.com>
2025-02-09 16:35:10 +0100

    Updates to auth controller

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -10,10 +10,10 @@ export const signup = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log(errors.array());
+    return res.status(400).json({ errors: errors.array() });
   }
 
-  const hashedPassword = bcrypt.hash(password, 12);
+  const hashedPassword = await bcrypt.hash(password, 12);
 
   const user = new User({
     name: name,
