5a2a984344223f256a0321a5611c0f5dc3801730
kifo99 <ivan123rakovic@gmail.com>
2025-02-09 16:32:12 +0100

    Adding updates to Signup component

--- client/src/components/Modal/Signup.jsx
+++ client/src/components/Modal/Signup.jsx
@@ -1,10 +1,30 @@
 // import AddIcon from "../../assets/icons/add/add.svg";
 import PropTypes from "prop-types";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
 export default function Signup({ showSignup }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
   const signupRef = useRef();
 
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await axios.get(`http://localhost:8080/admin/signup`, formData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   useEffect(
     function () {
       if (showSignup && signupRef.current) {
@@ -28,22 +48,31 @@ export default function Signup({ showSignup }) {
         <h1 className="text-amber-300 font-bold mb-4 text-center text-3xl p-7 ">
           Sign Up
         </h1>
-        <form className="flex flex-col justify-center items-center space-y-4 ">
+        <form
+          onSubmit={(e) => handleSubmit(e)}
+          className="flex flex-col justify-center items-center space-y-4 "
+        >
           <input
             type="text"
             name="name"
+            value={formData.name}
+            onChange={(e) => handleChange(e)}
             placeholder=" Name"
             className=" m-9  w-3/4 h-11 rounded-lg bg-amber-200 text-amber-800 border-none focus:border-amber-800  focus:ring-amber-800 focus:ring-2 outline-none focus:bg-amber-300 "
           />
           <input
             type="email"
             name="email"
+            value={formData.email}
+            onChange={(e) => handleChange(e)}
             placeholder=" Email"
             className=" m-3  w-3/4 h-11 rounded-lg bg-amber-200 text-amber-800 border-none focus:border-amber-800  focus:ring-amber-800 focus:ring-2 outline-none focus:bg-amber-300 "
           />
           <input
             type="password"
             name="password"
+            value={formData.password}
+            onChange={(e) => handleChange(e)}
             placeholder=" Password"
             className=" m-9  w-3/4 h-11 rounded-lg bg-amber-200 text-amber-800 border-none focus:border-amber-800  focus:ring-amber-800 focus:ring-2 outline-none focus:bg-amber-300 "
           />
