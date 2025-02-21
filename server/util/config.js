d4a886850b2d799275bbfd3d9db39ebd31719bf1
kifo99 <ivan123rakovic@gmail.com>
2025-02-21 19:42:20 +0100

    Adding new route and controler for handeling login

--- server/util/config.js
+++ server/util/config.js
@@ -1,3 +1,5 @@
 export const PORT = 8080;
 export const MONGODB_URL =
   "mongodb+srv://ivan123rakovic:mlbt2706MdLe@nyanime-lsit.ja1wt.mongodb.net/animelist?retryWrites=true&w=majority&appName=nyanime-lsit";
+
+export const SECRET_KEY = "iwanttobeasupersaiyan";
