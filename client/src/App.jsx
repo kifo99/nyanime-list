2e16820cdbe73f7354aa5c8436b8057ebc824aa1
kifo99 <ivan123rakovic@gmail.com>
2025-02-23 19:26:10 +0100

    Adding new features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -61,6 +61,7 @@ export default function App() {
         <Signup
           showSignup={showSignup}
           onShowSignup={setShowSignup}
+          onSetHasAccount={setHasAccount}
           navRef={navRef}
         />
       </div>
