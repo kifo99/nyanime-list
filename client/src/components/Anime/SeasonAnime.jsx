c452eda3925697c805b256bbd77e0fcd8c14ae5e
kifo99 <ivan123rakovic@gmail.com>
2025-03-06 18:56:11 +0100

    Adding updates on SeasonAnime component

--- client/src/components/Anime/SeasonAnime.jsx
+++ client/src/components/Anime/SeasonAnime.jsx
@@ -22,11 +22,9 @@ export default function SeasonAnime() {
     fetchData();
   }, []);
 
-  console.log("This is inside of SeasonAnime");
-
   return (
     <div className="flex justify-center items-center">
-      <ul className="flex flex-row justify-between overflow-hidden scroll-smooth">
+      <ul className="flex flex-row justify-between overflow-x-auto scroll-smooth scrollbar-hide">
         {anime.map((anime) => (
           <SeasonAnimeCard anime={anime} key={anime.id} />
         ))}
--- client/src/components/Anime/SeasonAnimeCard.jsx
+++ client/src/components/Anime/SeasonAnimeCard.jsx
@@ -1,19 +1,19 @@
 import PropTypes from "prop-types";
 
 export default function SeasonAnimeCard({ anime }) {
-  console.log(`${anime.title} id: ${anime.id}`);
-
   return (
-    <li className="flex justify-center items-center w-2xs m-3">
-      <div className="">
+    <li className="min-w-[400px] m-3">
+      <div className="flex justify-center items-center">
         <img
           src={anime.image}
           alt={anime.title}
-          className="rounded-lg shadow-lg w-10 h-24"
+          className="rounded-lg shadow-lg min-w-[180] min-h-[250] m-auto"
         />
       </div>
-      <div>
-        <h1 className="text-xs font-semibold text-gray-800">{anime.title}</h1>
+      <div className="flex justify-center items-center">
+        <h1 className="text-2xl font-semibold text-gray-800 m-auto">
+          {anime.title}
+        </h1>
       </div>
     </li>
   );
