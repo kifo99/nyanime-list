642008b43d2de938d14b4a1843ab7dbe2067cce8
kifo99 <ivan123rakovic@gmail.com>
2025-03-06 17:12:21 +0100

    Adding new updates on anime controller

--- server/controller/anime.js
+++ server/controller/anime.js
@@ -131,16 +131,23 @@ export const getSeasonAnime = async (req, res, next) => {
     const { data } = await axios.get(`https://api.jikan.moe/v4/seasons/now`);
 
     if (!data) throw new Error("Failed to fetch data.");
+    const seenIds = new Set();
 
-    const animeList = [];
-
-    data.data.forEach((anime) => {
-      animeList.push({
-        id: anime.mal_id,
-        image: anime.images.jpg.image_url,
-        title: anime.title,
+    const animeList = data.data
+      .filter((anime) => {
+        if (!seenIds.has(anime.mal_id)) {
+          seenIds.add(anime.mal_id);
+          return true;
+        }
+        return false;
+      })
+      .map((anime) => {
+        return {
+          id: anime.mal_id,
+          image: anime.images.jpg.image_url,
+          title: anime.title_english,
+        };
       });
-    });
 
     res.status(200).json({
       message: "Top 10 anime list is fetched",
