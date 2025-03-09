5ad339eba155b7db00a378f4c3c36bd4168af1dc
kifo99 <ivan123rakovic@gmail.com>
2025-03-09 16:45:35 +0100

    Adding new changes to components and new functionality

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 
@@ -7,6 +7,8 @@ import SearchIcon from "../../assets/icons/search/search.svg";
 export default function Search({ onSetAnimeList }) {
   const [animeName, setAnimeName] = useState("");
   const [searchRes, setSearchRes] = useState("");
+
+  const inputEl = useRef(null);
 
   function handleSearchAnime(e) {
     setSearchRes(e.target.value);
@@ -17,6 +19,18 @@ export default function Search({ onSetAnimeList }) {
     setAnimeName(searchRes);
     setSearchRes("");
   }
+
+  useEffect(() => {
+    function callback(e) {
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+
+    return () => document.addEventListener("keydown", callback);
+  }, []);
 
   useEffect(
     function () {
@@ -48,22 +62,23 @@ export default function Search({ onSetAnimeList }) {
   );
 
   return (
-    <div className="flex  p-2 justify-content items-center w-full">
+    <form
+      onSubmit={(e) => handleSubmitAnime(e)}
+      className="flex  p-2 justify-content items-centerw-full"
+    >
       <input
+        ref={inputEl}
         type="text"
         placeholder="Search..."
-        className="w-full h-14 py-1 font-semibold px-4 text-xl rounded-full focus:outline-none bg-amber-300 text-amber-50"
+        className="flex justify-content items-center w-full h-14 font-semibold px-4 text-xl rounded-full focus:outline-none bg-amber-300 text-amber-50 placeholder-amber-100 leading-none"
         value={searchRes}
         onChange={(e) => handleSearchAnime(e)}
       />
 
-      <button
-        className="ml-2 h-10 w-10 bg-amber-300 p-2 rounded-full hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-bg-amber-300 transition-all items-center"
-        onClick={(e) => handleSubmitAnime(e)}
-      >
+      <button className="ml-2 h-10 w-10 bg-amber-300 p-2 rounded-full hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-amber-bg-amber-300 transition-all items-center">
         <img width={24} height={24} src={SearchIcon} alt="svg search icon" />
       </button>
-    </div>
+    </form>
   );
 }
 
