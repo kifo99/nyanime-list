ed88d049fbc8dacf46a6fa5649f02bb1e6682491
kifo99 <ivan123rakovic@gmail.com>
2025-04-15 19:17:49 +0200

    Adding new changes to server side of nyanimelist

--- server/controller/watchlist.js
+++ server/controller/watchlist.js
@@ -1,19 +1,43 @@
 import { Watchlist } from "../model/watchlist.js";
 import { User } from "../model/user.js";
-import mongoose from "mongoose";
+import { errorHandler } from "../util/helpers.js";
 
 export const addToWatchList = async (req, res, next) => {
-  const { userId, animeId } = req.params;
+  try {
+    const { userId, animeId } = req.params;
+    const user = await User.findById(userId);
 
-  const user = await User.findById(userId);
-  const watchlist = await Watchlist.findById(user.watchlistId);
+    if (!user) {
+      throw errorHandler(null, "User doesn't exist!", 404);
+    }
 
-  let tempWatchlist;
+    const watchlist = await Watchlist.findById(user.watchlistId);
 
-  console.log(user.watchlistId);
+    if (watchlist && watchlist.userId.toString() === user._id.toString()) {
+      const alreadyAdded = watchlist.items.some(
+        (item) => item.animeId === animeId
+      );
 
-  if (!watchlist) {
-    tempWatchlist = new Watchlist({
+      if (!alreadyAdded) {
+        watchlist.items.push({
+          animeId: animeId,
+          addedOn: Date.now(),
+        });
+        await watchlist.save();
+
+        return res.status(200).json({
+          message: "Anime added to watchlist",
+          watchlist: watchlist,
+        });
+      }
+
+      return res.status(200).json({
+        message: "Anime already in watchlist",
+        watchlist: watchlist,
+      });
+    }
+
+    const tempWatchlist = new Watchlist({
       userId: user._id,
       items: [
         {
@@ -25,10 +49,42 @@ export const addToWatchList = async (req, res, next) => {
 
     user.watchlistId = tempWatchlist._id;
 
-    await user.save();
+    await Promise.all([tempWatchlist.save(), user.save()]);
 
-    console.log("USerrrrrrrr");
+    return res.status(200).json({
+      message: "Watchlist created and anime added to watchlist",
+      watchlist: tempWatchlist,
+    });
+  } catch (err) {
+    res.status(err.statusCode || 500).json({
+      message: err.message || "Something is wrong pleas try again later",
+    });
   }
+};
 
-  //   console.log(user);
+export const getWatchlist = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.findById(userId);
+
+    if (!user) throw errorHandler(null, "User not founded!", 404);
+
+    const watchlist = await Watchlist.findById(user.watchlistId);
+
+    if (!watchlist) throw errorHandler(null, "Watchlist not founded!", 404);
+
+    const animeList = watchlist.items.map((item) => item.animeId);
+
+    console.log(animeList);
+
+    res.status(200).json({
+      message: "Watchlist found",
+      watchlist: animeList,
+    });
+  } catch (err) {
+    res.status(err.statusCode || 500).json({
+      message: err.message || "Something is wrong pleas try again later",
+    });
+  }
 };
--- server/routes/watchlist.js
+++ server/routes/watchlist.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { addToWatchList } from "../controller/watchlist.js";
+import { addToWatchList, getWatchlist } from "../controller/watchlist.js";
 
 const router = express.Router();
 
+router.get("/get/:userId", getWatchlist);
 router.post("/add/:userId/:animeId", addToWatchList);
 
 export default router;
