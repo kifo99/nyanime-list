10cd91bbf9fc305623093ad88c85eebca4989f2e
kifo99 <ivan123rakovic@gmail.com>
2025-04-28 14:45:19 +0200

    Updating watchlist UI

--- client/src/components/Anime/WatchlistCard.jsx
+++ client/src/components/Anime/WatchlistCard.jsx
@@ -1,13 +1,27 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { Trash2, CircleX } from "lucide-react";
+
 export default function WatchlistCard({ anime }) {
+  const [showDeleteBtn, setShowDeleteBtn] = useState(false);
+
   return (
     <Link
-      className="block m-1 border border-transparent hover:border-solid hover:border-rose-600 hover:border-2 hover:rounded-lg"
+      className="relative block m-1 border border-transparent hover:border-solid hover:border-gray-600 hover:border-2 hover:rounded-lg overflow-visible"
       to={`/anime/${anime.animeId}`}
+      onMouseEnter={() => setShowDeleteBtn(true)}
+      onMouseLeave={() => setShowDeleteBtn(false)}
     >
-      <div className="flex-row gap-1.5 ">
+      {showDeleteBtn && (
+        <CircleX
+          className="absolute top-0 right-0  rounded-full p-1 text-red-600 hover:text-red-500 hover:w-11 hover:h-11"
+          size={40}
+          strokeWidth={2.5}
+        />
+      )}
+      <div className=" flex-row gap-1.5 ">
         <img src={anime.image} className="m-auto w-24 h-36" />
         <h1 className="text-gray-600 font-extrabold text-xl m-auto text-center">
           {anime.title || anime.titleJapanese}
