648c044e29081b69b53f468eea33e79bc6298c50
kifo99 <ivan123rakovic@gmail.com>
2025-04-28 16:17:27 +0200

    New changes to WatchlistCard UI

--- client/src/components/Anime/WatchlistCard.jsx
+++ client/src/components/Anime/WatchlistCard.jsx
@@ -1,33 +1,57 @@
 import PropTypes from "prop-types";
+import axios from "axios";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import { Trash2, CircleX } from "lucide-react";
+import useAuthStore from "../../store/useAuthStore";
 
 export default function WatchlistCard({ anime }) {
   const [showDeleteBtn, setShowDeleteBtn] = useState(false);
 
+  const { userId, token } = useAuthStore();
+
+  async function handelRemoveAnime() {
+    try {
+      await axios.delete(
+        `http://localhost:8080/watchlist/delete/${userId}/${anime.animeId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <Link
-      className="relative block m-1 border border-transparent hover:border-solid hover:border-gray-600 hover:border-2 hover:rounded-lg overflow-visible"
-      to={`/anime/${anime.animeId}`}
+    <div
+      className="relative p-4 group"
       onMouseEnter={() => setShowDeleteBtn(true)}
       onMouseLeave={() => setShowDeleteBtn(false)}
     >
       {showDeleteBtn && (
         <CircleX
-          className="absolute top-0 right-0  rounded-full p-1 text-red-600 hover:text-red-500 hover:w-11 hover:h-11"
-          size={40}
+          className="absolute top-1 right-1 p-1 text-red-600 hover:text-red-500 transition-all opacity-90 hover:opacity-100 cursor-pointer"
+          size={32}
           strokeWidth={2.5}
+          onClick={handelRemoveAnime}
         />
       )}
       <div className=" flex-row gap-1.5 ">
-        <img src={anime.image} className="m-auto w-24 h-36" />
+        <Link to={`/anime/${anime.animeId}`}>
+          <img
+            src={anime.image}
+            className="border border-transparent hover:border-solid hover:border-gray-600 hover:border-2 hover:rounded-lg m-auto w-24 h-36 hover:w-32 hover:h-44"
+          />
+        </Link>
         <h1 className="text-gray-600 font-extrabold text-xl m-auto text-center">
           {anime.title || anime.titleJapanese}
         </h1>
       </div>
-    </Link>
+    </div>
   );
 }
 
