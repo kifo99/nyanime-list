4aec0cc3b1d31792f979b16d36451c10731bc23a
kifo99 <ivan123rakovic@gmail.com>
2025-05-24 15:25:08 +0200

    Adding changes to MyProfile page

--- client/src/pages/Profile/MyProfile.jsx
+++ client/src/pages/Profile/MyProfile.jsx
@@ -1,5 +1,11 @@
 export default function MyProfile() {
-  return <div>
-    
-  </div>;
+  return (
+    <div className="flex justify-between items-center w-full">
+      <div className="bg-amber-400 w-1/3 m-1">
+        <div>Profile pic</div>
+        <div>User info</div>
+      </div>
+      <div className="bg-rose-500 w-2/3 m-1">Feed</div>
+    </div>
+  );
 }
