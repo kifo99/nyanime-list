de1b9824e4807a369618752ae49c25500bfcd4a2
kifo99 <ivan123rakovic@gmail.com>
2025-06-03 20:45:41 +0200

    Adding new functionality to profile and user controllers

--- server/controller/user.js
+++ server/controller/user.js
@@ -19,3 +19,33 @@ export const getUser = async (req, res, next) => {
     });
   }
 };
+
+export const updateProfilePic = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const newProfilePic = req.file;
+
+    let profilePicPath;
+    if (newProfilePic) {
+      profilePicPath = `/images/avatar/${newProfilePic.filename}`;
+    }
+
+    const user = await User.findById(userId);
+    if (!user) errorHandler(null, "User not found!", 404);
+
+    console.log(user.avatar);
+
+    user.avatar = profilePicPath;
+
+    await user.save();
+
+    res.status(200).json({
+      message: "Profile picture updated.",
+      user: user,
+    });
+  } catch (err) {
+    res.status(err.statusCode || 500).json({
+      message: err.message,
+    });
+  }
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,15 @@
 import express from "express";
 
-import { getUser } from "../controller/user.js";
+import { getUser, updateProfilePic } from "../controller/user.js";
+import imageUpload from "../middleware/imageUpload.js";
 
 const router = express.Router();
 
 router.get("/profile/:userId", getUser);
+router.put(
+  "/:userId/profile-picture",
+  imageUpload.single("newProfilePic"),
+  updateProfilePic
+);
 
 export default router;
